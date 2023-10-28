eef84598d999229781158f56b28d3668daaf5226
Adilbek <zhumazhanov40@gmail.com>
2023-10-28 16:00:25 +0600

    first

---
+++ src/pages/Home.js
@@ -0,0 +1,10 @@
+const Home = () => {
+    return (
+        <>
+        <h2>It is Home page, teacher. I was a sick and late for 1 hour. i have been written this code about 30min. </h2> 
+        <input type="text" name="input" value="Whats happening?" />
+        </>
+    );
+  };
+  
+  export default Home;
---
+++ src/pages/Profile.js
@@ -0,0 +1,9 @@
+const Profile = () => {
+    return (
+        <>
+            <h2>Adilbek Zhumazhanov</h2>
+        </>
+    );
+  };
+  
+  export default Profile;
